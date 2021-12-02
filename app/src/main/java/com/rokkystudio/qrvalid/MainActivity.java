ae9a80245a4a245cb076720a368d2d0773517222
RokkyStudio <rokkystudio@gmail.com>
2021-12-03 00:18:05 +0300

    1

--- app/src/main/java/com/rokkystudio/qrvalid/MainActivity.java
+++ app/src/main/java/com/rokkystudio/qrvalid/MainActivity.java
@@ -1,17 +1,27 @@
 package com.rokkystudio.qrvalid;
 
+import static android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.KeyEvent;
+import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.zxing.BarcodeFormat;
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -23,10 +33,11 @@ import java.util.Collections;
 
 public class MainActivity extends Activity
 {
+    private static final int CAMERA_REQUEST_CODE = 100;
+
     private QuaredBarcodeView mBarcodeView;
     private String mLastBarcode;
     private Vibrator mVibrator;
-
     private WebView mWebView;
 
     @Override
@@ -43,14 +54,41 @@ public class MainActivity extends Activity
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
 
-        mBarcodeView = findViewById(R.id.BarcodeView);
-        if (mBarcodeView == null) return;
-
-        Collection<BarcodeFormat> formats = Collections.singletonList(BarcodeFormat.QR_CODE);
-        mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formats));
-        mBarcodeView.decodeContinuous(new MyBarcodeCallback());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(MIXED_CONTENT_ALWAYS_ALLOW);
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
+        }
 
         mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        mBarcodeView = findViewById(R.id.BarcodeView);
+        if (mBarcodeView == null) return;
+        Collection<BarcodeFormat> formats = Collections.singletonList(BarcodeFormat.QR_CODE);
+        mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formats));
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA }, CAMERA_REQUEST_CODE);
+        } else {
+            startScanner();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScanner();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    private void startScanner() {
+        if (mBarcodeView != null) {
+            mBarcodeView.decodeContinuous(new MyBarcodeCallback());
+        }
     }
 
     @Override
